f015fb8ef134a836fdc17ad6274a8261fefc09c5
Brian DeBoer <bdeboer@cosm.com>
2023-05-16 09:11:14 -0700

    fix: some documenting

--- activities-cancellation-heartbeating/src/workflows.rs
+++ activities-cancellation-heartbeating/src/workflows.rs
@@ -13,8 +13,10 @@ pub async fn run_cancellable_activity(mut ctx: WfContext) -> WorkflowResult<u64>
 
     let fake_progress_handle = ctx.activity(ActivityOptions {
         activity_type: "fake_progress_activity".to_string(),
-        cancellation_type: ActivityCancellationType::TryCancel,
+        cancellation_type: ActivityCancellationType::WaitCancellationCompleted,
         input: 500.as_json_payload().expect("Unable to serialize"), // payload here is the sleep time
+        // Note that how long you set the timeout to will impact when the cancellation for the activity is processed
+        // if you make it too long, it'll just keep going until the end.
         heartbeat_timeout: Some(Duration::from_secs(3)),
         start_to_close_timeout: Some(Duration::from_secs(120)),
         ..Default::default()
@@ -32,6 +34,8 @@ pub async fn run_cancellable_activity(mut ctx: WfContext) -> WorkflowResult<u64>
     let cancel_handle = ctx.cancelled();
 
     let mut final_value = 0;
+
+    // tokio select waits on multiple asyncs and returns after the FIRST one completes
     tokio::select!(
         _ = cancel_handle => {
             warn!("## workflow canceled ##");
